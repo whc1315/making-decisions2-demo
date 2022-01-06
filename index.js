57da9a1266c23e3d29ffa066f097e9e52340a8dc
William Cuthbertson <whc1315@gmail.com>
2022-01-06 11:17:11 -0700

    BAckpack and Belt

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log (`hello world`)
+
+let backpack = []
+
+backpack.push('sword', 'potion')
+backpack.push('food')
+backpack.push('shield')
+
+
+let onBelt = backpack.splice(0,1)
+
+//console.log(backpack)
+//console.log(onBelt)
+console.log('backpack:', backpack, 'Belt: ', onBelt)
